342946130315fbb7fcc4a02854872fc009a208d1
Prelang Builder <builder@prelang.com>
2015-07-06 04:21:06 +0000

    Defined validation for attribute 'author_url' on model 'Project'

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,5 @@ class Project < ActiveRecord::Base
   has_and_belongs_to_many :tags
   has_many :instructions
   validates_formatting_of :thumb_url, using: :url
+  validates_formatting_of :author_url, using: :url
 end
