248e46ffb0b974472dedee97ada2847195b979ee
Prelang Builder <builder@prelang.com>
2015-07-06 04:21:19 +0000

    Setting the User of the model on #create to the currently signed in user

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -29,6 +29,7 @@ class BlogPostsController < ApplicationController
   # POST /blog_posts.json
   def create
     @blog_post = BlogPost.new(blog_post_params)
+    @blog_post.user = current_user
 
     respond_to do |format|
       if @blog_post.save
